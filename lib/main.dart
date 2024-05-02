0fd1f9ef9796865d167247199641d71debcbf528
anagh <anaghamk7@gmail.com>
2024-05-02 20:04:58 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,20 @@
-
+import 'dart:io';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:go_tech_solutions/page_1.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  runApp(const MyApp());
+  WidgetsBinding.instance.addPostFrameCallback((timeStamp) async{
+    if (Platform.isAndroid) {
+      await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+    }
+  });
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
