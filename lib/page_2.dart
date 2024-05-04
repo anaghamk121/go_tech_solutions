548b72a1fa7bbc00c46d7e5d6a622be757afcd68
anagh <anaghamk7@gmail.com>
2024-05-04 20:12:54 +0530

    commit

--- lib/page_2.dart
+++ lib/page_2.dart
@@ -1,26 +1,103 @@
-
-
 import 'package:flutter/material.dart';
+import 'package:flutter_windowmanager/flutter_windowmanager.dart';
 import 'package:go_tech_solutions/page_3.dart';
 
-class Page2 extends StatelessWidget {
+class Page2 extends StatefulWidget {
   @override
+  State<Page2> createState() => _Page2State();
+}
+
+class _Page2State extends State<Page2> {
+
+
+  @override
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Page 2'),
+        title: const Text(' Page 2'),
       ),
       body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.push(
-                 context,
-                 MaterialPageRoute(builder: (context) => Page3()),
-             );
-          },
-          child: const Text('Go to Page 3')
+        child: Column(
+          children: [
+            const Text(
+              'Screen Recording Disabled:',
+              style: TextStyle(fontSize: 18.0),
+            ),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Page3()),
+                  );
+                },
+                child: const Text('Go to Page 3')),
+          ],
         ),
       ),
     );
   }
 }
+
+//
+// import 'package:flutter/material.dart';
+// import 'package:flutter_windowmanager/flutter_windowmanager.dart';
+//
+//
+// class Page2 extends StatefulWidget {
+//   @override
+//   _ScreenRecordingDemoState createState() => _ScreenRecordingDemoState();
+// }
+//
+// class _ScreenRecordingDemoState extends State<Page2> {
+//   bool screenRecordingDisabled = false;
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     disableScreenRecording();
+//   }
+//
+//   Future<void> disableScreenRecording() async {
+//     try {
+//       await FlutterWindowManager.addFlags(FlutterWindowManager.FLAG_SECURE);
+//       setState(() {
+//         screenRecordingDisabled = true;
+//       });
+//     } catch (e) {
+//       print('Failed to disable screen recording: $e');
+//       setState(() {
+//         screenRecordingDisabled = false;
+//       });
+//     }
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Screen Recording Disable Demo'),
+//       ),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: <Widget>[
+//             Text(
+//               'Screen Recording Disabled:',
+//               style: TextStyle(fontSize: 18.0),
+//             ),
+//             SizedBox(height: 10.0),
+//             Text(
+//               screenRecordingDisabled ? 'Yes' : 'No',
+//               style: TextStyle(
+//                 fontSize: 24.0,
+//                 fontWeight: FontWeight.bold,
+//                 color: screenRecordingDisabled ? Colors.green : Colors.red, // Updated here
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
